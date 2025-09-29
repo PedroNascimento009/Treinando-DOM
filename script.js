2501936a68449e5f2accf1129ba327349b5be198
Pedro Nascimento <pedro.nascimento00099@gmail.com>
2025-09-29 13:06:26 -0300

    InsertBefore
    Inserindo elementos

---
+++ script.js
@@ -0,0 +1,9 @@
+let elemento = document.querySelector('#titulo')
+
+const p = document.createElement('p')
+
+const header = elemento.parentElement
+
+header.insertBefore(p, elemento)
+
+console.log(header)
