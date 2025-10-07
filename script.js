b27603e53d3ecf4b358119b76c862656f7a1ab04
Pedro Nascimento <pedro.nascimento00099@gmail.com>
2025-10-07 14:10:12 -0300

    load

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const input = document.querySelector('#myInput');
-
-input.addEventListener('focus', () => {
-    input.style.backgroundColor = 'lightblue';
-});
-
-input.addEventListener('blur', () => {
-    input.style.backgroundColor = 'blue';
-});
+window.addEventListener('load', function() {
+    console.log('Pagina carregada');
+});          
