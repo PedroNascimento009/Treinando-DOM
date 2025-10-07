ca32ab336297357883a8eecaeb96ba08498524af
Pedro Nascimento <pedro.nascimento00099@gmail.com>
2025-10-07 15:09:52 -0300

    debounce

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-window.addEventListener('load', function() {
-    console.log('Pagina carregada');
-});          
+function debounce(func, delay) {
+  let timeoutId;
+    return function (...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
+}
+
+window.addEventListener('mousemove', debounce((event) => {
+    console.log(`Mouse moved to: (${event.clientX}, ${event.clientY})`);
+}, 100));
